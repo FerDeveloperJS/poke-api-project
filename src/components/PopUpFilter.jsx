586b609193375d3e8d9e90f868f2e01d02f6b4d4
FerDeveloperJS <ferchosoftwaredev@gmail.com>
2025-09-22 12:37:01 -0500

    Refactor Pop Up Filter Component

--- src/components/PopUpFilter.jsx
+++ src/components/PopUpFilter.jsx
@@ -2,66 +2,42 @@ import PokemonType from "./PokemonType";
 import CloseSvg from "../assets/svg/CloseSvg";
 
 function PopUpFilter({ setPopUp }) {
+  const pokemonTypeArray = [
+    { text: "NORMAL", bgColor: "#A8A77A" },
+    { text: "FIRE", bgColor: "#EE8130" },
+    { text: "WATER", bgColor: "#6390F0" },
+    { text: "ELECTRIC", bgColor: "#F7D02C" },
+    { text: "GRASS", bgColor: "#7AC74C" },
+    { text: "ICE", bgColor: "#96D9D6" },
+    { text: "FIGHTING", bgColor: "#C22E28" },
+    { text: "POISON", bgColor: "#A33EA1" },
+    { text: "GROUND", bgColor: "#E2BF65" },
+    { text: "FLYING", bgColor: "#A98FF3" },
+    { text: "PSYCHIC", bgColor: "#F95587" },
+    { text: "BUG", bgColor: "#A6B91A" },
+    { text: "ROCK", bgColor: "#B6A136" },
+    { text: "GHOST", bgColor: "#735797" },
+    { text: "DRAGON", bgColor: "#6F35FC" },
+    { text: "DARK", bgColor: "#705746" },
+    { text: "STEEL", bgColor: "#B7B7CE" },
+    { text: "FAIRY", bgColor: "#D685AD" },
+  ];
   return (
     <div className="fixed z-20 top-0 left-1/2 -translate-x-1/2 py-10 rounded-2xl bg-white max-w-[700px] mx-auto">
       <h2 className="text-[#FFCB05] font-[Onest] font-bold text-2xl md:text-4xl text-center mb-10">
         Filtro por tipo
       </h2>
       <ul className="flex flex-wrap gap-5 justify-center">
-        <li>
-          <PokemonType text={"NORMAL"} bgColor={"#A8A77A"} />
-        </li>
-        <li>
-          <PokemonType text={"FIRE"} bgColor={"#EE8130"} />
-        </li>
-        <li>
-          <PokemonType text={"WATER"} bgColor={"#6390F0"} />
-        </li>
-        <li>
-          <PokemonType text={"GRASS"} bgColor={"#7AC74C"} />
-        </li>
-        <li>
-          <PokemonType text={"ELECTRIC"} bgColor={"#F7D02C"} />
-        </li>
-        <li>
-          <PokemonType text={"ICE"} bgColor={"#96D9D6"} />
-        </li>
-        <li>
-          <PokemonType text={"FIGTHING"} bgColor={"#C22E28"} />
-        </li>
-        <li>
-          <PokemonType text={"POISON"} bgColor={"#A33EA1"} />
-        </li>
-        <li>
-          <PokemonType text={"GROUND"} bgColor={"#E2BF65"} />
-        </li>
-        <li>
-          <PokemonType text={"FLYING"} bgColor={"#A98FF3"} />
-        </li>
-        <li>
-          <PokemonType text={"PSYCHIC"} bgColor={"#F95587"} />
-        </li>
-        <li>
-          <PokemonType text={"BUG"} bgColor={"#A6B91A"} />
-        </li>
-        <li>
-          <PokemonType text={"ROCK"} bgColor={"#B6A136"} />
-        </li>
-        <li>
-          <PokemonType text={"GHOST"} bgColor={"#735797"} />
-        </li>
-        <li>
-          <PokemonType text={"DRAGON"} bgColor={"#6F35FC"} />
-        </li>
-        <li>
-          <PokemonType text={"DARK"} bgColor={"#705746"} />
-        </li>
-        <li>
-          <PokemonType text={"STEEL"} bgColor={"#B7B7CE"} />
-        </li>
-        <li>
-          <PokemonType text={"FAIRY"} bgColor={"#D685AD"} />
-        </li>
+        {pokemonTypeArray.map((pokemonType) => {
+          return (
+            <li key={pokemonType.text}>
+              <PokemonType
+                text={pokemonType.text}
+                bgColor={pokemonType.bgColor}
+              />
+            </li>
+          );
+        })}
       </ul>
       <CloseSvg setPopUp={setPopUp} />
     </div>
