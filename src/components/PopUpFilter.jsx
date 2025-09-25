557649b4435a978a3f13bc965b9c83a2fdb70293
FerDeveloperJS <ferchosoftwaredev@gmail.com>
2025-09-25 08:55:50 -0500

    Improved responsive pop up filter component design

--- src/components/PopUpFilter.jsx
+++ src/components/PopUpFilter.jsx
@@ -4,8 +4,8 @@ import pokemonTypeArray from "../assets/utils/PokemonTypeArray";
 
 function PopUpFilter({ setPopUp, setActualURL, setFilter }) {
   return (
-    <div className="fixed z-20 top-0 left-1/2 -translate-x-1/2 py-10 rounded-2xl bg-white max-w-[700px] mx-auto">
-      <h2 className="text-[#FFCB05] font-[Onest] font-bold text-2xl md:text-4xl text-center mb-10">
+    <div className="fixed z-20 top-0 left-1/2 -translate-x-1/2 py-10 rounded-2xl bg-white min-w-[300px] max-w-[700px] mx-auto">
+      <h2 className="text-[#FFCB05] font-[Onest] font-bold text-2xl md:text-4xl text-center mb-7">
         Filtro por tipo
       </h2>
       <ul className="flex flex-wrap gap-5 justify-center">
